e196744732b63e204d426812fc95d45d4e13539d
Jay Maddox <jaymaddox5384@gmail.com>
2020-10-31 11:57:18 -0500

    new2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,14 +1,105 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,Button, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,Button, Breadcrumb, BreadcrumbItem, Modal,ModalHeader,ModalBody,Row,Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { render } from 'node-sass';
+import { Control } from 'react-redux-form';
+import { LocalForm } from 'react-redux-form';
+import { Errors } from 'react-redux-form';
+
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+
+
 
 class CommentForm extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+
+        
+        this.state = {
+            
+            isModalOpen: false
+        };
+        
+        this.toggleModal = this.toggleModal.bind(this);
+        
+    }
+    
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    
+    
     render(){
         return(
             
+            
         <div>                  
-            <Button type="submit"><i className="fa fa-pencil fa-lg"></i>Submit Comment</Button>
+            <Button onClick={this.toggleModal} outline color="secondary"><i className="fa fa-pencil fa-lg"></i>Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} >
+                <ModalHeader>Login</ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Col md={10}>
+                                <Control.select modal=".rating" name="rating"
+                                   className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Col>
+
+                            <Col>
+                                <Control.text model=".firstName" id="firstName"
+                                    placeholder=" First Name"
+                                    className="form-control">
+                                        validators={{
+                                            required, 
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
+                                        <Errors
+                                        className="text-danger"
+                                        model=".firstName"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+
+                                </Control.text>
+                                
+                            </Col>
+
+                            <Col>
+                               <Control.textarea modal=".text" id="text">
+
+                               </Control.textarea>
+                            </Col>
+                        </Row>
+                        
+                        <Row className="form-group">
+                                <Col md={{size: 10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                        Send Feedback
+                                    </Button>
+                                </Col>
+                            </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
         </div>                   
             
         );
@@ -50,13 +141,14 @@ function RenderComments({comments}) {
                             <div key={comment.id}>
                                 <p>
                                     {comment.text}<br />
-                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', 
+                                    { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                                 </p>
                             </div>
                         );
                     })
                 }
-                
+                <CommentForm/>
             </div>
 
            
@@ -65,7 +157,11 @@ function RenderComments({comments}) {
         );
     }
     
-    return  <div/>;
+    return (
+        <div/>
+
+    )
+    
     
    
 
